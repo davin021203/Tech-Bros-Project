4aa07f1387ae45ecb2cf9db8feb705449cd7eb6d
davin021203 <1211201162@student.mmu.edu.my>
2023-01-26 11:17:56 +0800

    done

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,7 +3,7 @@ const createNav = () => {
 
     nav.innerHTML = `
         <div class="nav">
-            <img src="img/dark - logo.png" class="brand-logo" alt="">
+            <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
@@ -15,12 +15,10 @@ const createNav = () => {
         </div>
         <ul class="links-container">
             <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">storage</a></li>
-            <li class="link-item"><a href="#" class="link">cooling</a></li>
-            <li class="link-item"><a href="#" class="link">processors</a></li>
-            <li class="link-item"><a href="#" class="link">RAM</a></li>
-            <li class="link-item"><a href="#" class="link">Accecories</a></li>
-
+            <li class="link-item"><a href="#" class="link">women</a></li>
+            <li class="link-item"><a href="#" class="link">men</a></li>
+            <li class="link-item"><a href="#" class="link">kids</a></li>
+            <li class="link-item"><a href="#" class="link">accessories</a></li>
         </ul>
     `;
 }
---
+++ public/js/product.js
@@ -0,0 +1,26 @@
+const productImages = document.querySelectorAll(".product-images img");
+const productImageSlide = document.querySelector(".image-slider");
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        productImages[activeImageSlide].classList.remove('active');
+        item.classList.add('active');
+        productImageSlide.style.backgroundImage = `url('${item.src}')`;
+        activeImageSlide = i;
+    })
+})
+
+// toggle size buttons
+
+const sizeBtns = document.querySelectorAll('.size-radio-btn');
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizeBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    })
+})
