09214aa4b948f7be98b30a30ed40d43ef1d4b7a4
davin021203 <1211201162@student.mmu.edu.my>
2023-02-01 20:37:27 +0800

    DONE

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,24 +2,24 @@ const createNav = () => {
     let nav = document.querySelector('.navbar');
 
     nav.innerHTML = `
-        <div class="nav">
-            <img src="img/dark-logo.png" class="brand-logo" alt="">
-            <div class="nav-items">
-                <div class="search">
-                    <input type="text" class="search-box" placeholder="search brand, product">
-                    <button class="search-btn">search</button>
-                </div>
-                <a href="#"><img src="img/user.png" alt=""></a>
-                <a href="#"><img src="img/cart.png" alt=""></a>
-            </div>
+    <div class="nav">
+    <img src="img/logo_transparent.png" class="brand-logo" alt="">
+    <div class="nav-items">
+        <div class="search">
+            <input type="text" class="search-box" placeholder="search brand, product">
+            <button class="search-btn">search</button>
         </div>
-        <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">kids</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
-        </ul>
+        <a href="#"><img src="img/user.png" alt=""></a>
+        <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+</div>
+<ul class="links-container">
+    <li class="link-item"><a href="#" class="link">Home</a></li>
+    <li class="link-item"><a href="#" class="link">Motherboard</a></li>
+    <li class="link-item"><a href="#" class="link">RAM</a></li>
+    <li class="link-item"><a href="#" class="link">Solid-State Drives</a></li>
+    <li class="link-item"><a href="#" class="link">Others</a></li>
+</ul>
     `;
 }
 
