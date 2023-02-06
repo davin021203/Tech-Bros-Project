83fe847771a14d6eb816171f0e0775aba93b40bb
davin021203 <103306247+davin021203@users.noreply.github.com>
2023-02-06 21:28:42 +0800

    Add files via upload

---
+++ Ecoomerce Website/admin_area/insert_brands.php
@@ -0,0 +1,35 @@
+<?php
+include('../includes/connect.php');
+if(isset($_POST['insert_brand'])){
+  $brand_title=$_POST['brand_title'];
+
+  //select data  from database
+  $select_query="Select * from brands where brand_title='$brand_title'";
+  $result_select=mysqli_query($con,$select_query);
+  $number=mysqli_num_rows($result_select);
+  if($number>0){
+    echo "<script>alert('This brand is present in the database')</script>";
+  }
+  $insert_query="insert into brands (brand_title) values ('$brand_title')";
+  $result=mysqli_query($con,$insert_query);
+  if($result){
+    echo "<script>alert('Brand has been inserted successfully')</script>";
+  }
+}
+?>
+<h2 class="text-center">Insert Brands</h2>
+
+
+<form action="" method="post" class="mb-2">
+<div class="input-group w-90 mb-2">
+  <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-received"></i></span>
+  <input type="text" class="form-control" name="brand_title" placeholder="Insert Brands"  aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group w-10 mb-2 m-auto">
+  
+ <input type="submit" class=" bg-info border-0 p-2 my-3" name="insert_brand" value="Insert Brands" aria-describedby="basic-addon1" class="bg-info">
+  
+</div>
+
+</form>
